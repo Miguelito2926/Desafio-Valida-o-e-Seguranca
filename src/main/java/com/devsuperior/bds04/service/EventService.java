f7d74c0f8a22ebcb85ec5948867ec19d6304e043
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-09-17 22:59:28 -0300

    implements EventController, EventService and EventRepository

---
+++ src/main/java/com/devsuperior/bds04/service/EventService.java
@@ -0,0 +1,41 @@
+package com.devsuperior.bds04.service;
+
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventService {
+
+	@Autowired
+	private EventRepository eventRepository;
+
+	@Transactional(readOnly = true)
+	public Page<EventDTO> findAllPaged(Pageable pageable) {
+		Page<Event> list = eventRepository.findAll(pageable);
+		return list.map(EventDTO::new);
+	}
+
+	@Transactional
+	public EventDTO insert(EventDTO dto) {
+		Event entity = new Event();
+		copyDtoToEntity(dto, entity);
+		entity = eventRepository.save(entity);
+		return new EventDTO(entity);
+	}
+
+	private void copyDtoToEntity(EventDTO dto, Event entity) {
+
+		entity.setName(dto.getName());
+		entity.setDate(dto.getDate());
+		entity.setUrl(dto.getUrl());
+		entity.setCity(new City(dto.getCityId(), null));
+	}
+}
