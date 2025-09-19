e99bc75a2093b66e7503d8c355a1d2be24a99e5a
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-09-18 21:50:41 -0300

    UserDetailsProjection and DTOS

---
+++ src/main/java/com/devsuperior/bds04/dto/UserInsertDTO.java
@@ -0,0 +1,37 @@
+package com.devsuperior.bds04.dto;
+
+
+import com.devsuperior.bds04.entities.User;
+import com.devsuperior.bds04.service.validation.UserInsertValid;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@UserInsertValid
+public class UserInsertDTO extends UserDTO implements Serializable {
+	@Serial
+	private static final long serialVersionUID = 1L;
+
+	private String password;
+
+	public UserInsertDTO () {
+		super();
+	}
+
+	public UserInsertDTO(String password) {
+		this.password = password;
+	}
+
+	public UserInsertDTO(User entity, String password) {
+		super(entity);
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/main/java/com/devsuperior/bds04/dto/UserUpdateDTO.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bds04.dto;
+
+
+import com.devsuperior.bds04.service.validation.UserUpdateValid;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@UserUpdateValid
+public class UserUpdateDTO  extends UserDTO implements Serializable {
+	@Serial
+	private static final long serialVersionUID = 1L;
+}
