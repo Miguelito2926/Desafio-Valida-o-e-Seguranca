778c5880b7f98f9672561ff55f141da7a5d707b0
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-09-18 21:51:33 -0300

    Validation UserInsertValidator

---
+++ src/main/java/com/devsuperior/bds04/service/validation/UserInsertValid.java
@@ -0,0 +1,19 @@
+package com.devsuperior.bds04.service.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Constraint(validatedBy = UserInsertValidator.class)
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UserInsertValid {
+    String message() default "Validation error";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
